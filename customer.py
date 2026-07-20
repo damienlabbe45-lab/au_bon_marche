712ddffdee176c82d29396f94e008d5b83e26eb8
Séverine HM <severine.hori.maitrehut@fms-ea.com>
2026-07-20 13:32:26 +0200

    Ajout des fonctions init et repr à la classe Customer

--- customer.py
+++ customer.py
@@ -5,3 +5,14 @@ from dataclasses import dataclass
 class Customer:
     first_name: str
     last_name: str
+
+
+def __init__(self, first_name, last_name):
+    self.first_name = first_name
+    self.last_name = last_name
+
+
+def __repr__(self):
+    nom = type(self).__name__
+    return f"{nom}{self.last_name} - {self.first_name}"
+
