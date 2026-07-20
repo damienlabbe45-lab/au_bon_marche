6b44a5f6d45f8f597c6c6206c82e10dbc0ee20ac
Damien Labbe <damien.labbe@fms-ea.com>
2026-07-20 13:47:05 +0200

    ajout des docstring pour les classes Vegetables , Vegetablesbykg ainsi qu'à leurs fonctions

--- legume.py
+++ legume.py
@@ -2,22 +2,28 @@ from typing import Self
 
 
 class Vegetables():
-
+    """classe des fruits et légumes"""
     def __init__(self: Self, name_vegetable: str) -> None:
+        """on initialise la classe avec son nom"""
         self.name_vegetable = name_vegetable
 
     def __repr__(self: Self):
+        """on indique pour le print, on fait juste son nom"""
         print(self.name_vegetable)
 
 
 class Vegetablebykg(Vegetables):
+    """il s'agit des fruits et légumes qu'on met avec le poids"""
     def __init__(self: Self, name_vegetable: str, weight: float) -> None:
+        """on appelle la méthode la classe mère puis ensuite, on rajoute le poids"""
         super().__init__(name_vegetable)
         self.weight = weight
 
     def __repr__(self: Self):
+        """on appelle la méthode de la classe mère puis on rajoute le poids"""
         super().__repr__()
         print(self.weight)
 
     def weight_minus(self: Self, weight_minus) -> None:
+        """la méthode permet de dimunier le poids. sera utile pour actualiser le poids si des clients en prend """
         self.weight -= weight_minus
