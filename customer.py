721ebd6c7740b3959dfe0fa515a5aadccf06588a
Séverine HM <severine.hori.maitrehut@fms-ea.com>
2026-07-20 14:19:15 +0200

    Mise à jour docstring dans customer.py

--- customer.py
+++ customer.py
@@ -3,16 +3,19 @@ from dataclasses import dataclass
 
 @dataclass
 class Customer:
+    """Classe Client"""
     first_name: str
     last_name: str
 
 
 def __init__(self, first_name, last_name):
+    """ Méthode pour instancier un nouveau client, paramètres attendus first_name, et last_name """
     self.first_name = first_name
     self.last_name = last_name
 
 
 def __repr__(self):
+    """ Méthode pour représenter un client """
     nom = type(self).__name__
     return f"{nom}{self.last_name} - {self.first_name}"
 
