e368ee44b1371e0752c9b4fcc62b678316a2e195
Séverine HM <severine.hori.maitrehut@fms-ea.com>
2026-07-20 14:21:47 +0200

    Mise à jour docstring dans line_order_basket.py

--- line_order_basket.py
+++ line_order_basket.py
@@ -4,10 +4,12 @@ from legume import Vegetables
 
 @dataclass
 class LineOrderBasket:
+    """Class Ligne Panier : représente une ligne d'un panier de commande"""
     vegetables_ordered: Vegetables
     quantity_ordered: int
     sub_total: float
 
 
 def __init__(self: LineOrderBasket, vegetables: Vegetables):
+    """ Initialisation d'une ligne d'un panier en définissant déjà le fruit ou légume choisi"""
     self.vegetables = vegetables
