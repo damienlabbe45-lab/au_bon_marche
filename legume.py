7f1d7a1d6cdde487b8e46ae6803159d7bbbadc0b
Damien Labbe <damien.labbe@fms-ea.com>
2026-07-20 14:33:29 +0200

    modification de __repr__ dans chacune des classes et correction des fautes d'orthographes dans les docstrings

--- legume.py
+++ legume.py
@@ -1,31 +1,53 @@
 from typing import Self
 
 
-class Vegetables():
+class Vegetables:
     """classe des fruits et légumes"""
+
     def __init__(self: Self, name_vegetable: str, price: float) -> None:
         """on initialise la classe avec son nom et son prix"""
         self.name_vegetable = name_vegetable
         self.price = price
 
     def __repr__(self: Self):
-        """on indique pour le print, on fait juste son nom puis ensuite le prix"""
-        print(self.name_vegetable)
-        print(self.price)
+        """on indique pour le print, on fait juste son nom puis le prix"""
+        return f"{self.name_vegetable} \n pour {self.price} euros"
 
 
 class Vegetablebykg(Vegetables):
     """il s'agit des fruits et légumes qu'on met avec le poids"""
-    def __init__(self: Self, name_vegetable: str, weight: float) -> None:
-        """on appelle la méthode la classe mère puis ensuite, on rajoute le poids"""
-        super().__init__(name_vegetable)
+
+    def __init__(self: Self, name_vegetable: str, price, weight: float) -> None:
+        """on appelle la méthode la classe mère puis, on rajoute le poids"""
+        super().__init__(name_vegetable, price)
         self.weight = weight
 
     def __repr__(self: Self):
-        """on appelle la méthode de la classe mère puis on rajoute le poids"""
-        super().__repr__()
-        print(self.weight)
+        """on ajoute le poids puis d'avant d'appeler la méthode de la classe mère"""
+        return f"{self.weight} de {super().__repr__()}"
 
-    def weight_minus(self: Self, weight_minus) -> None:
-        """la méthode permet de dimunier le poids. sera utile pour actualiser le poids si des clients en prend """
-        self.weight -= weight_minus
+    def weight_minus(self: Self, weight_minus: float) -> None:
+        """la méthode permet de diminuer le poids. Sera utile pour actualiser le poids si des clients en prennent """
+        if self.weight >= weight_minus:
+            self.weight -= weight_minus
+        else:
+            print(f"il me reste juste que {self.weight}.")
+
+
+class Vegetableperpiece(Vegetables):
+    """on appelle la méthode la classe mère puis on ajoute le nombre d'unités"""
+    def __init__(self: Self, name_vegetable: str, price, unit: int) -> None:
+        super().__init__(name_vegetable, price)
+        self.unit = unit
+
+    def __repr__(self: Self):
+        """on met le nombre d'unités puis on appelle la classe mère"""
+        return f"{self.unit} {super().__repr__()}"
+
+    def piece_minus(self: Self, piece_minus: int) -> None:
+        """méthode pour diminuer le nombre d'unités. Utile pour actualiser le nombre de pieces si des clients en achète
+        """
+        if self.unit >= piece_minus:
+            self.unit -= piece_minus
+        else:
+            print(f"je n'ai pas assez de d'unités. j'en ai que {self.unit}")
