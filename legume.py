82495812274d120f99b5aa301aadcdb30f832a1c
Damien Labbe <damien.labbe@fms-ea.com>
2026-07-20 13:27:52 +0200

    fichier legume.py avec respect de pep8

--- legume.py
+++ legume.py
@@ -1,8 +1,10 @@
 from typing import Self
 
+
 class Vegetables():
-    def __init__(self:Self, name_vegatble : str) -> None:
+    def __init__(self: Self, name_vegatble: str) -> None:
         self.name_vegatble = name_vegatble
 
-    def __repr__(self:Self):
+    def __repr__(self: Self):
         print(self.name_vegatble)
+
