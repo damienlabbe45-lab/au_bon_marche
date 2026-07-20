65e7a2e912a4cf7a414ec7d279785e24d061dec0
Séverine HM <severine.hori.maitrehut@fms-ea.com>
2026-07-20 14:16:51 +0200

    Création de la méthode init pour la classe OrderBasket

--- order_basket.py
+++ order_basket.py
@@ -4,5 +4,10 @@ from line_order_basket import LineOrderBasket
 
 @dataclass
 class OrderBasket:
+    ref_order_basket: int
     list_line_order: list[LineOrderBasket]
     total: float
+
+
+def __init__(self, ref_order_basket: int):
+    self.ref_order_basket = ref_order_basket
