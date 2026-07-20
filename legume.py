de732e1ec69ad2f31549208b88fc72f7dc91fb0d
Damien Labbe <damien.labbe@fms-ea.com>
2026-07-20 13:38:07 +0200

    création de la classe Vegetablebykg ainsi que de ses fonctions __init__, __repr__ et weigth_minus

--- legume.py
+++ legume.py
@@ -2,9 +2,22 @@ from typing import Self
 
 
 class Vegetables():
-    def __init__(self: Self, name_vegatble: str) -> None:
-        self.name_vegatble = name_vegatble
+
+    def __init__(self: Self, name_vegetable: str) -> None:
+        self.name_vegetable = name_vegetable
 
     def __repr__(self: Self):
-        print(self.name_vegatble)
+        print(self.name_vegetable)
 
+
+class Vegetablebykg(Vegetables):
+    def __init__(self: Self, name_vegetable: str, weight: float) -> None:
+        super().__init__(name_vegetable)
+        self.weight = weight
+
+    def __repr__(self: Self):
+        super().__repr__()
+        print(self.weight)
+
+    def weight_minus(self: Self, weight_minus) -> None:
+        self.weight -= weight_minus
