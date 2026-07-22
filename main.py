7e4ecd7ccb85e1f39fba13f16a23154032ba375e
Damien Labbe <damien.labbe@fms-ea.com>
2026-07-22 15:18:07 +0200

    utilisation de la fonction input_client

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from vendeur import Merchant
 from legume import init_vegetables
 from functions_customers import get_arrival_customer
+from fonctions_vendeurs import input_client
+
 
 def main():
-    merchant = Merchant(name="Théodore", list_vegetable = init_vegetables())
-    client = get_arrival_customer()
+    merchant = Merchant(name="Théodore", list_vegetable=init_vegetables())
+    customer = get_arrival_customer()
     merchant.offer()
-
-
-
+    input_client(merchant, customer)
 
 
 if __name__ == "__main__":
