c8780f1d64773e19da72f5e13072304c7eb3bd7c
Damien Labbe <damien.labbe@fms-ea.com>
2026-07-22 11:05:14 +0200

    début de la fonction main et mis du if __name__ == '__main__': en appelant cette même fonction main()

---
+++ main.py
@@ -0,0 +1,12 @@
+from vendeur import Merchant
+from legume import init_vegetables
+
+
+def main():
+    merchant = Merchant(name="Théodore", list_vegetable=init_vegetables())
+
+
+
+
+if __name__ == "__main__":
+    main()
