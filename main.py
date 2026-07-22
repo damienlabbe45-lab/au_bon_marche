5ace7db9aaa82d75eac23a137025b1a5e2b28cc6
Damien Labbe <damien.labbe@fms-ea.com>
2026-07-22 11:36:12 +0200

    on continue la fonction main

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from vendeur import Merchant
 from legume import init_vegetables
-
+from functions_customers import get_arrival_customer
 
 def main():
-    merchant = Merchant(name="Théodore", list_vegetable=init_vegetables())
+    merchant = Merchant(name="Théodore", list_vegetable = init_vegetables())
+    client = get_arrival_customer()
+    merchant.offer()
+
 
 
 
