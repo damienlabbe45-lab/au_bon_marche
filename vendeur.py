852d9564958d8c3a0d5cf26f90741fea1679ab28
Damien Labbe <damien.labbe@fms-ea.com>
2026-07-22 10:16:32 +0200

    création de la classe Merchant avec son constructeur

---
+++ vendeur.py
@@ -0,0 +1,18 @@
+from legume import Vegetablebykg, Vegetableperpiece
+from typing import Self
+
+
+class Merchant:
+    """classe pour vendre les fruits et légumes"""
+
+    def __init__(self: Self, name: str, list_vegetable: list[Vegetableperpiece | Vegetablebykg]) -> None:
+        self.name = name
+        self.vegetable = list_vegetable
+        self.receipt: list[str] = []
+        self.monnaie: float = 0
+        self.sold: list[Vegetableperpiece | Vegetablebykg] = []
+
+
+
+
+
