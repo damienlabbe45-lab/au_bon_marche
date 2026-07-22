493ac28e2f6ba0d5c9b5bb69ad101620cc76798c
Damien Labbe <damien.labbe@fms-ea.com>
2026-07-22 14:13:45 +0200

    modifications des while dans les fonctions

--- fonctions_vendeurs.py
+++ fonctions_vendeurs.py
@@ -3,11 +3,12 @@ from legume import Vegetableperpiece, Vegetablebykg
 from functions_customers import add_vegetable_to_order_basket_customer
 from customer import Customer
 from re import match
+from operator import attrgetter
 
 
 def input_client_vegetable(text, merchant: Merchant) -> str:
     text_user = input(text)
-    while text_user not in merchant.vegetable:
+    while text_user not in list(map(attrgetter("name"), merchant.vegetable)):
         text_user = input(text)
     return text_user
 
@@ -18,7 +19,7 @@ def input_client_kg(text, vegetable: Vegetablebykg) -> float:
         text_user = float(text_user)
     else:
         text_user = 1000000
-    while vegetable.weigth >= text_user:
+    while vegetable.weight < text_user:
         text_user = input(text)
         if bool(match(r"^\d+([;,]\d*)?$", text_user)):
             text_user = float(text_user)
@@ -33,10 +34,13 @@ def input_client_piece(text, vegetable: Vegetableperpiece) -> int:
         text_user = int(text_user)
     else:
         text_user = 1000000
-    while vegetable.unit >= text_user:
+    while vegetable.unit < text_user:
         text_user = input(text)
         if text_user.isdigit():
             text_user = int(text_user)
         else:
             text_user = 1000000
     return text_user
+
+
+def input_client(merchant:Merchant, customer:Customer) -> None:
