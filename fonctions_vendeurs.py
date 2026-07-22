3036987497f2d48a61bbbcd2078fd4aa9e6fdf01
Damien Labbe <damien.labbe@fms-ea.com>
2026-07-22 13:53:20 +0200

    ajout de la fonction input_client_piece

--- fonctions_vendeurs.py
+++ fonctions_vendeurs.py
@@ -25,3 +25,18 @@ def input_client_kg(text, vegetable: Vegetablebykg) -> float:
         else:
             text_user = 1000000
     return text_user
+
+
+def input_client_piece(text, vegetable: Vegetableperpiece) -> int:
+    text_user = input(text)
+    if text_user.isdigit():
+        text_user = int(text_user)
+    else:
+        text_user = 1000000
+    while vegetable.unit >= text_user:
+        text_user = input(text)
+        if text_user.isdigit():
+            text_user = int(text_user)
+        else:
+            text_user = 1000000
+    return text_user
