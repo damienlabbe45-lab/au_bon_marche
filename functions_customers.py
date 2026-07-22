04b3005afd91257c4cf9faab0f6d4e05e7e50eb8
Séverine HM <severine.hori.maitrehut@fms-ea.com>
2026-07-22 11:08:45 +0200

    Ajout du fichier functions_customers, et ajout des fonctions pour l'input nom/prénom

---
+++ functions_customers.py
@@ -0,0 +1,23 @@
+""" Séverine Hori Maitrehut
+Fonctions qui concernent le menu pour l'arrivée du client
+"""
+
+def is_saisie_str_ok(saisie):
+    """Fonction qui vérifie si une saisie chaine nom/prénom est correcte"""
+    cleaned_saisie = saisie.strip()
+    if not cleaned_saisie.isalpha():
+        return False
+    return True
+
+def get_str_input(prompt):
+    """Fonction qui demande au joueur de saisir son nom"""
+    input_user = input(prompt)
+    while not is_saisie_str_ok(input_user):
+        input_user = input("Saisie incorrecte. Merci de recommencer : ")
+    return input_user
+
+
+def get_menu_client():
+    print("Arrivée d'un client")
+    customer_last_name = get_str_input("Entrez le nom du client: ")
+    customer_first_name = get_str_input("Entrez le prénom du client: ")
